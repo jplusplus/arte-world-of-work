b7923d8123e95de4446ff3a53367cc91f2644cdb
pbellon <bellon.pierre@gmail.com>
2014-01-14 11:35:55 +0100

    Changed core modelisation and removed usage of polymorphic

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,8 +1,13 @@
+from django import forms
 from django.contrib import admin
-from app.core.models import Question
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.admin import TabularInline, StackedInline
+from django.contrib.contenttypes import generic
+
+from app.core.models import Question, Typology
 
 class QuestionAdmin(admin.ModelAdmin):
-    pass 
+    fields = ('label', 'hint_text', 'typology_type')
 
 # Register your models here.
 admin.site.register(Question, QuestionAdmin)
--- app/core/models.py
+++ app/core/models.py
@@ -1,20 +1,23 @@
 from django.db import models
 from django.utils.translation import ugettext as _
 
-from polymorphic import PolymorphicModel
+import re
+
+
+# from polymorphic import PolymorphicModel
 
 # -----------------------------------------------------------------------------
 # 
 #     Choices field types
 # 
 # -----------------------------------------------------------------------------
-class BaseChoiceField(PolymorphicModel):
+class BaseChoiceField(models.Model):
     title = models.CharField(_('Title of this choice'), max_length=120)
 
 class TextChoiceField(BaseChoiceField):
     pass
 
-class MediaChoiceField(BaseChoiceField, PolymorphicModel):
+class MediaChoiceField(BaseChoiceField):
     pass
 
 class ImageChoiceField(MediaChoiceField):
@@ -28,17 +31,55 @@ class IconChoiceField(MediaChoiceField):
 #     Typologies
 # 
 # -----------------------------------------------------------------------------
-class Typology(PolymorphicModel):
-    pass
+class TypologyManager(models.Manager):
 
-class BaseMultipleChoicesTypology(Typology, PolymorphicModel):
+    def inject_types(self, types):
+        self.types = types
+
+    def type_from_klass(self, kls): 
+        return re.sub( '(?<!^)(?=[A-Z])', '_', kls.__name__).lower()
+
+    def klass_from_type(self, type):
+        assert self.is_typology_type(type)
+        return eval("".join(
+                map(
+                    lambda x: x.capitalize(),
+                    type.split('_')
+                )
+            )
+        )
+
+
+    def is_typology_type(self, type):
+        is_safe_name = len(re.findall('[\W]', type)) == 0
+        is_in_subtypes = filter(lambda x: x[0] == type, self.types) is not None 
+        return is_in_subtypes and is_safe_name
+
+    def create_typology(self, typology_type):
+        typology_klass = self.klass_from_type(typology_type)
+        typology = typology_klass()
+        typology.save()
+        return typology
+
+
+
+
+class Typology(models.Model):
+    sub_type = models.CharField(_('Typology subtype'), max_length=30)
+    objects = TypologyManager()
+
+    def get_subclass(self):
+        return self.objects.klass_from_type(self.sub_type)
+
+
+class BaseMultipleChoicesTypology(Typology):
     choices = models.ManyToManyField('BaseChoiceField')
 
 class SelectionTypology(BaseMultipleChoicesTypology):
     # multiple choices allowed
     value = models.ManyToManyField('BaseChoiceField')
 
-class RadioTypology(BaseMultipleChoicesTypology, PolymorphicModel):
+class RadioTypology(BaseMultipleChoicesTypology):
     # single value 
     value = models.ForeignKey(BaseChoiceField)
 
@@ -47,8 +88,8 @@ class BooleanTypology(RadioTypology):
     def create(klass):
         choices  = (
             # default yes choice,
-            BaseChoiceField(title=_('yes')),
-            BaseChoiceField(title=_('no')),
+            TextChoiceField(title=_('yes')),
+            TextChoiceField(title=_('no')),
             # default no choice
         )
         typology = klass()
@@ -56,7 +97,22 @@ class BooleanTypology(RadioTypology):
         return typology
 
 
+TYPOLOGIES_TYPES = (
+    ( Typology.objects.type_from_klass(SelectionTypology), _('Multiple choices question (1 or more answer)')),
+    ( Typology.objects.type_from_klass(RadioTypology),     _('Radio choices question (1 answer only)'      )),
+    ( Typology.objects.type_from_klass(BooleanTypology),   _('Boolean choice question (1 answer only)'     ))
+)
+
+Typology.objects.inject_types(TYPOLOGIES_TYPES)
+
 class Question(models.Model):
-    label = models.CharField(_('Question label'), max_length=120)
+    label = models.CharField(_('Question label'), max_length=220)
     hint_text = models.CharField(_('Question hint text'), max_length=120)
-    typology = models.ForeignKey(Typology)
+    typology_type = models.CharField(_('Question typology'), max_length=30, choices=TYPOLOGIES_TYPES)
+    typology = models.OneToOneField(Typology, primary_key=True)
+
+    def save(self, *args, **kwargs):
+        if self.typology_type:
+            self.typology = Typology.objects.create_typology(self.typology_type)
+        super(Question, self).save()
+
