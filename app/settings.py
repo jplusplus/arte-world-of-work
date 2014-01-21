cb9940c8350f46713a3755d2aac145c8f231a626
pbellon <bellon.pierre@gmail.com>
2014-01-21 15:03:45 +0100

    Added translations to installed app

--- app/settings.py
+++ app/settings.py
@@ -151,6 +151,7 @@ INSTALLED_APPS = (
     'south',
     # ------------------------ internal dependencies ------------------------ # 
     'app.core',
+    'app.translations',
 )
 
 # A sample logging configuration. The only tangible logging
