947c0b8821082226adcd77e58adb78dae240e6e4
pbellon <bellon.pierre@gmail.com>
2014-01-27 14:57:05 +0100

    Setup compressor to work with S3 on heroku

--- app/settings_heroku.py
+++ app/settings_heroku.py
@@ -25,7 +25,7 @@ STATIC_URL                 = os.getenv('STATIC_URL')
 
 STATIC_ROOT                = here('staticfiles')
 
-STATICFILES_DIRS           = ()
+STATICFILES_DIRS           = (here('static'))
 
 INSTALLED_APPS            += ('storages',)
 
