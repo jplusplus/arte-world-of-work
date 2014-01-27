638553db1a310a2ae288044fd3bfa089e978e1c7
pbellon <bellon.pierre@gmail.com>
2014-01-27 17:35:22 +0100

    Changed ugettext by ugettext_lazy in models

--- app/core/models.py
+++ app/core/models.py
@@ -16,7 +16,7 @@ from django.contrib.contenttypes.models import ContentType
 from django.core.exceptions import ValidationError
 from django.db import models
 from django.db.models.signals import post_save
-from django.utils.translation import ugettext as _
+from django.utils.translation import ugettext_lazy as _
 
 from django_countries.fields import CountryField
 from sorl.thumbnail import ImageField
