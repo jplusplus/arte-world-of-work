c0695c1d1f7f065efe3c770601e5811823cdab69
pbellon <bellon.pierre@gmail.com>
2014-01-28 11:04:28 +0100

    Fixing backoffice (SITE_ID setting key was deleted)

--- app/settings.py
+++ app/settings.py
@@ -37,6 +37,8 @@ DATABASES = {
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = []
 
+SITE_ID = 1
+
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
