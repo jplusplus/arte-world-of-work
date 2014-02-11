74aeb723e7e004299ab36b9019b8fbb26dfeb4d1
pbellon <bellon.pierre@gmail.com>
2014-02-11 10:48:58 +0100

    Fixing travis build

--- app/settings_tests.py
+++ app/settings_tests.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = (
     'rest_framework',
     'sorl.thumbnail',
     # ------------------------ internal dependencies ------------------------ # 
+    'app.authentication',
     'app.core',
     'app.translations',
 )
