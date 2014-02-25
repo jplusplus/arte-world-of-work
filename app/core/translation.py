f62bd2a4c76ffcf95527fd621d8c72ffcf3d39c0
pbellon <bellon.pierre@gmail.com>
2014-02-25 17:06:23 +0100

    Added core translation file with registering of core models translations

--- app/core/translation.py
+++ app/core/translation.py
@@ -1,12 +1,30 @@
-# from app.core import models
-# from app.translations.translator import translator, TranslationOptions
+# -*- coding: utf-8 -*-
 
-# # question translation
-# class FeedbackTransOpts(TranslationOptions):
-#     pass
+from app.core import models
+from app.translations.translator import translator, TranslationOptions
 
-# class QuestionTransOpts(TranslationOptions):
-#     fields = ('')
+# question translation
+class QuestionTransOpts(TranslationOptions):
+    fields = ('label', 'hint_text', 'skip_button_label')
 
-# class ThematicTransOpts(TranslationOptions):
-#     fields = ('title', 'intro_description', 'intro_button_label')
+class ChoiceFieldTransOpts(TranslationOptions):
+    fields = ('title', )
+
+class TypedNumberQuestionTransOpts(TranslationOptions):
+    fields = ('unit', )
+
+class ThematicTransOpts(TranslationOptions):
+    fields = ('title', 'intro_description', 'intro_button_label')
+
+class FeedbackTransOpts(TranslationOptions):
+    fields = ('html_sentence', )
+
+class StaticFeedbackTransOpts(TranslationOptions):
+    fields = ('source_title', )
+
+translator.register(models.BaseQuestion,        QuestionTransOpts)
+translator.register(models.TypedNumberQuestion, TypedNumberQuestionTransOpts)
+translator.register(models.BaseChoiceField,     ChoiceFieldTransOpts)
+translator.register(models.Thematic,            ThematicTransOpts)
+translator.register(models.BaseFeedback,        FeedbackTransOpts)
+translator.register(models.StaticFeedback,      StaticFeedbackTransOpts)
