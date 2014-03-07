9249390057ffd91f4a8a0d953f27e5915700ec0a
pbellon <bellon.pierre@gmail.com>
2014-03-07 12:04:48 +0100

    Set back our custom user model

--- app/settings.py
+++ app/settings.py
@@ -189,7 +189,7 @@ INSTALLED_APPS = (
     'app.translations',
 )
 
-# AUTH_USER_MODEL = 'authentication.WWUser'
+AUTH_USER_MODEL = 'authentication.WWUser'
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error when DEBUG=False.
