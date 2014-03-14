4b7d2d2797a465ddc45037e26bda2e834a9f7e68
pbellon <bellon.pierre@gmail.com>
2014-03-14 15:29:43 +0200

    Added back auth user model & authentication app to installed app

--- app/settings.py
+++ app/settings.py
@@ -167,7 +167,7 @@ COMPRESS_ENABLED = False
 
 INSTALLED_APPS = (
     # contain user model, install it first
-    # 'app.authentication',
+    'app.authentication',
     # ------------------------ django dependencies -------------------------- # 
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -189,7 +189,7 @@ INSTALLED_APPS = (
     'app.translations',
 )
 
-# AUTH_USER_MODEL = 'authentication.WWUser'
+AUTH_USER_MODEL = 'authentication.WWUser'
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error when DEBUG=False.
