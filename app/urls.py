711fe5c6b9cd1662a447c701944ca62ccdd17e4d
pbellon <bellon.pierre@gmail.com>
2014-04-10 13:03:03 +0200

    Added header for app/{urls, utils, views & wsgi}.py files

--- app/urls.py
+++ app/urls.py
@@ -1,15 +1,33 @@
 #!/usr/bin/env python
 # Encoding: utf-8
 # -----------------------------------------------------------------------------
-# Project : Arte - WoW
+# Project: World of Work
 # -----------------------------------------------------------------------------
-# Author : 
+# Author: Pierre Bellon                               <bellon.pierre@gmail.com>
 # -----------------------------------------------------------------------------
-# License : proprietary journalism++
+# License: GNU General Public License
 # -----------------------------------------------------------------------------
-# Creation : 14-Jan-2014
-# Last mod : 14-Jan-2014
+# Creation time:      2014-03-26 19:00:01
+# Last Modified time: 2014-04-10 13:01:51
 # -----------------------------------------------------------------------------
+# This file is part of World of Work
+# 
+#   World of Work is a study about european youth's perception of work
+#   Copyright (C) 2014 Journalism++
+#   
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#   
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#   
+#   You should have received a copy of the GNU General Public License
+#   along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 from django.conf import settings
 from django.conf.urls import patterns, include, url
 
--- app/views.py
+++ app/views.py
@@ -1,15 +1,33 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
+# Encoding: utf-8
 # -----------------------------------------------------------------------------
-# Project : Arte - WoW
+# Project: World of Work
 # -----------------------------------------------------------------------------
-# Author : 
+# Author: Pierre Bellon                               <bellon.pierre@gmail.com>
 # -----------------------------------------------------------------------------
-# License : proprietary journalism++
+# License: GNU General Public License
 # -----------------------------------------------------------------------------
-# Creation : 14-Jan-2014
-# Last mod : 14-Jan-2014
+# Creation time:      2014-04-07 17:49:13
+# Last Modified time: 2014-04-10 13:01:37
 # -----------------------------------------------------------------------------
+# This file is part of World of Work
+# 
+#   World of Work is a study about european youth's perception of work
+#   Copyright (C) 2014 Journalism++
+#   
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#   
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#   
+#   You should have received a copy of the GNU General Public License
+#   along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 from django.conf      import settings
 from django.http      import Http404
 from django.shortcuts import render_to_response, redirect
@@ -45,5 +63,3 @@ def partial(request, partial_name=None):
 def not_found(request):
     return redirect("/404/")
 
-
-# Create your views here.
